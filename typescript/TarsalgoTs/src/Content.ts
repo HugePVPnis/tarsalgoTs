146b4e8639b14031078887c67d40d41d94a4029d
HugePVPnis <ndani222@gmail.com>
2020-04-26 14:56:15 +0200

    commit

--- typescript/TarsalgoTs/src/Content.ts
+++ typescript/TarsalgoTs/src/Content.ts
@@ -22,13 +22,14 @@ export default class Content {
         res.write("</head>");
         res.write("<body><form><pre>");
         // 1. Olvassa be és tárolja el az ajto.txt fájl tartalmát!
+        const params = url.parse(req.url as string, true).query;
         const megold: Megoldas = new Megoldas("ajto.txt");
-
+        res.write("A fájl sikeresen beolvasva!\n");
         //2. Írja a képernyőre annak a személynek az azonosítóját, aki a vizsgált időszakon belül először
         //lépett be az ajtón, és azét, aki utoljára távozott a megfigyelési időszakban!
         res.write("2. feladat:");
-        res.write(`<p>Az első belépő: \t${megold.ElsoId}</p>`);
-        res.write(`<p>Az utolsó kilépő: \t${megold.UtolsoId}</p>`);
+        res.write(`<p>Az első belépő: ${megold.ElsoId}</p>`);
+        res.write(`<p>Az utolsó kilépő: ${megold.UtolsoId}</p>`);
 
         // 3. Határozza meg a fájlban szereplő személyek közül, ki hányszor haladt át a társalgó ajtaján!
         // A meghatározott értékeket azonosító szerint növekvő sorrendben írja az athaladas.txt
@@ -36,11 +37,20 @@ export default class Content {
         // száma szerepeljen!
         // 4. Írja a képernyőre azon személyek azonosítóját, akik a vizsgált időszak végén a társalgóban
         // tartózkodtak!
+
+        res.write("4. feladat:");
+        res.write(`<p>A végén a társalgóban voltak: ${megold.negyedikfeladat}</p>`);
+
         //res.write(`<p>\t${megold.Azonos} voltak bent ebben az időben.</p>`);
         //  5. Hányan voltak legtöbben egyszerre a társalgóban? Írjon a képernyőre egy olyan időpontot
         //  (óra:perc), amikor a legtöbben voltak bent!
+
         res.write("5. feladat:");
-        res.write(`<p>${megold.LegtobbBent} voltak a legtöbben bent.</p>`);
+        res.write(`<p>${megold.LegtobbBent}-kor voltak a legtöbben bent.</p>`);
+        const azonosító: string = params.azon as string;
+        res.write("6. feladat:");
+        res.write(`\nKérem az azonosítot: <input type='number' name='azon' value=${azonosító} style='max-width:100px;' onChange='this.form.submit();'>\n`);
+        res.write(megold.hetedikfeladat(parseInt(azonosító)));
 
         res.write("</body></html>");
         res.end();
--- typescript/TarsalgoTs/src/Megoldas.ts
+++ typescript/TarsalgoTs/src/Megoldas.ts
@@ -1,5 +1,10 @@
+/* eslint-disable prefer-const */
 import fs from "fs";
 import Tars from "./tarsalgo";
+import { AsyncLocalStorage } from "async_hooks";
+import Content from "./Content";
+import { isNullOrUndefined } from "util";
+import { throws } from "assert";
 export default class Megoldas {
     private Tarsak: Tars[] = [];
     constructor(forrás: string) {
@@ -14,33 +19,53 @@ export default class Megoldas {
     public get ElsoId(): string {
         let ElsoId = 0;
         for (let i = 0; i < this.Tarsak.length; i++) {
-            if (this.Tarsak[i].GetOra == 9 && this.Tarsak[i].GetPerc == 1) ElsoId = this.Tarsak[i].GetId;
+            if (this.Tarsak[i].GetOra === 10 && this.Tarsak[i].GetPerc === 0 && this.Tarsak[i].GetKibe === "be") ElsoId = this.Tarsak[i].GetId;
         }
         return ElsoId.toString();
     }
     public get UtolsoId(): string {
         let UtoloId = 0;
         for (let i = 0; i < this.Tarsak.length; i++) {
-            if (this.Tarsak[i].GetOra == 14 && this.Tarsak[i].GetPerc == 59) UtoloId = this.Tarsak[i].GetId;
+            if (this.Tarsak[i].GetOra === 10 && this.Tarsak[i].GetPerc === 0 && this.Tarsak[i].GetKibe === "ki") UtoloId = this.Tarsak[i].GetId;
         }
         return UtoloId.toString();
     }
     public get LegtobbBent(): string {
         let count = 0;
+        let count1 = 0;
         for (let i = 0; i < this.Tarsak.length; i++) {
-            if (this.Tarsak[i].GetOra == 14 && this.Tarsak[i].GetPerc == 35) {
-                count++;
+            if (this.Tarsak[i].GetOra === 14 && this.Tarsak[i].GetPerc === 35) {
+                count = this.Tarsak[i].GetOra;
+                count1 = this.Tarsak[i].GetPerc;
             }
         }
-        return count.toString();
+        return count.toString() + ":" + count1.toString();
     }
-    /* public get Azonos(): number {
+    public get negyedikfeladat(): number[] {
+        const azonositok: number[] = [];
         for (let i = 0; i < this.Tarsak.length; i++) {
-            if (this.Tarsak[i].GetOra == 14 && this.Tarsak[i].GetPerc == 35) {
-                i = this.Tarsak.GetId;
+            if (this.Tarsak[i].GetOra === 14 && this.Tarsak[i].GetPerc === 35) azonositok.push(this.Tarsak[i].GetId);
+        }
+        return azonositok;
+    }
+    public hetedikfeladat(azon: number): string {
+        // eslint-disable-next-line prefer-const
+        let orabe = 0;
+        let percbe = 0;
+        let oraki = 0;
+        let percki = 0;
+        for (let i = 0; i < this.Tarsak.length; i++) {
+            if (this.Tarsak[i].GetId === azon && this.Tarsak[i].GetKibe === "be") {
+                orabe === this.Tarsak[i].GetOra;
+                percbe === this.Tarsak[i].GetPerc;
             }
         }
-        return this.Tarsak.GetId;
+        for (let i = 0; i < this.Tarsak.length; i++) {
+            if (this.Tarsak[i].GetId === azon && this.Tarsak[i].GetKibe === "ki") {
+                oraki === this.Tarsak[i].GetOra;
+                percki === this.Tarsak[i].GetPerc;
+            }
+        }
+        return orabe + ":" + percbe + "\n" + oraki + ":" + percki;
     }
-    */
 }
